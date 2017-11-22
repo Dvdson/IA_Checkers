5168a4c57718f265ce7f4c43ca4afafc6bc813c3
Davidson Lucas Novais Nicácio <dvdsoncomp@gmail.com>
2017-11-22 12:48:18 -0300

    Iniciando Criação do agente

---
+++ src/main/java/main/Agente1.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Agente1 {
+	
+	Neural net;
+
+	Agente1(Neural net) {
+		this.net = net;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void sensor(Integer X, Integer Y, Table table ){
+		
+	}
+	
+	public
+	
+	
+	
+}
---
+++ src/main/java/main/Agente2.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Agente2 {
+
+	
+	
+}
